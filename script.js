843a118e1beb45dd6450eb7406a40594bf4de97e
furqaan <khadimfurqaan786@gmail.com>
2024-04-26 23:02:37 +0100

    finished library project

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const submit = document.querySelector('.submit');
 const title = document.querySelector('#title');
 const pages = document.querySelector('#pages');
 const author = document.querySelector('#author');
+const read = document.querySelector('#read')
 const bookgrid = document.querySelector('.book-grid');
 
 function Book(title, author, pages, read){
@@ -25,7 +26,6 @@ Book.prototype.addCard = function(){
   let cardRead = document.createElement('div');
   let read = document.createElement('button');
   let del = document.createElement('button');
-  let edit = document.createElement('button');
   card.classList.add('card');
   cardDesc.classList.add('card-desc');
   cardButtons.classList.add('card-buttons');
@@ -35,7 +35,20 @@ Book.prototype.addCard = function(){
   cardRead.classList.add('card-read');
   read.classList.add('read');
   del.classList.add('delete');
-  edit.classList.add('edit');
+
+  read.addEventListener('click', () => {
+    if(this.read){
+      cardRead.textContent = 'not read'
+      this.read = false;
+    }
+    else {
+      cardRead.textContent = 'read'
+      this.read = true;
+    }
+  })
+  del.addEventListener('click', () => {
+    card.remove();
+  })
 
   cardTitle.textContent = this.title;
   cardAuthor.textContent = this.author;
@@ -44,7 +57,6 @@ Book.prototype.addCard = function(){
 
   read.textContent = 'Read';
   del.textContent = 'Delete';
-  edit.textContent = 'Edit';
 
   cardDesc.appendChild(cardTitle);
   cardDesc.appendChild(cardAuthor);
@@ -53,7 +65,6 @@ Book.prototype.addCard = function(){
 
   cardButtons.appendChild(read);
   cardButtons.appendChild(del);
-  cardButtons.appendChild(edit);
 
   card.appendChild(cardDesc);
   card.appendChild(cardButtons);
@@ -61,33 +72,36 @@ Book.prototype.addCard = function(){
   bookgrid.appendChild(card);
 }
 
+
+addBook.addEventListener('click', () => {
+  popupForm.style.display = 'flex';
+})
+
+cancel.addEventListener('click', () => {
+  popupForm.style.display = 'none';
+    title.value = '';
+    pages.value = '';
+    author.value = '';
+    read.checked = false
+    
+}) 
+
+submit.addEventListener('click', () => {
+  if(form.checkValidity()){
+    popupForm.style.display = 'none';
+    const newBook = new Book(title.value, author.value, pages.value, read.checked)
+    title.value = '';
+    pages.value = '';
+    author.value = '';
+    read.checked = false;
+    newBook.addCard()
+  }
+})
+
+if(popupForm.style.display === 'flex'){
+  
+}
+
+// example book
 const theHobbit = new Book('The Hobbit', 'George R.R Martin', 576, true);
-
-theHobbit.addCard()
-
-
-
-
-
-
-
-
-
-
-// addBook.addEventListener('click', () => {
-//   popupForm.style.display = 'flex';
-// })
-
-// cancel.addEventListener('click', () => {
-//   popupForm.style.display = 'none';
-// }) 
-
-// submit.addEventListener('click', () => {
-//   if(form.checkValidity()){
-//     popupForm.style.display = 'none';
-//     title.value = '';
-//     pages.value = '';
-//     author.value = '';
-
-//   }
-// })
+theHobbit.addCard();
